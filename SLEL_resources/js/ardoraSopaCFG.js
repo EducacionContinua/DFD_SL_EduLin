ac2c5fdb785b5dd591618f84e8b4f26701dc31aa
EducacionContinua <64606650+EducacionContinua@users.noreply.github.com>
2020-04-30 12:48:40 -0500

    Add files via upload

---
+++ SLEL_resources/js/ardoraSopaCFG.js
@@ -0,0 +1,37 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360; timeIni=360; timeBon=0;
+var score=0; scoreMax=1; scoreInc=1; scoreDec=0
+var successes=0; successesMax=1; attempts=1; attemptsMax=1;
+var tiTime=false;
+var tiTimeType=0;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var startTime; tiSuccesses=false; tiAttempts=false; tiScore=false;
+typeGame=1;
+var colorBack="#FFFDFD"; colorButton="#C0C0C0"; colorText="#000000"; colorSele="#808080";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="¡LO LOGRASTE, NO OLVIDES TOMAR CAPTURA DE PANTALLA!"; messageTime=""; isShowMessage=false;
+var urlOk=""; urlTime="";
+var goURLOk="_blank"; goURLTime="_blank"; 
+var borderOk="#008000"; borderTime="#FF0000"; borderAttempts="#FF0000";borderError="#FF0000";
+var imaSel=0; texSel=0;
+var board=[]; var xAnswer=5; answer=[]; rows=6; cols=10; xKey=-1; yKey=-1; animationX=[]; animationY=[]; animationPosX=[]; animationPosY=[]; animationLetter=[];
+var words=["QVVUT0RJU0NJUExJTkE","VEVDTk9MT0dJQQ","VklSVFVBTA","Rk9STw","RElTVEFOQ0lB","QU5VSUVT","RElHSVRBTA","VU5BTQ","VElFTVBPUw","U1VB"];
+var c=[14,10,7,4,9,6,7,4,7,3];
+var defs=["EL PERFIL DE UN ESTUDIANTE EN LINEA DESARROLLA ___________","ELEMENTO VITAL EN LA EDUCACION EN LINEA","UN AULA QUE NO REQUIERE UN ESPACIO FISICO SE CONSIDERA UN AULA _______","HERRAMIENTA DE LA EDUCACION QUE PERMITE COMPARTIR IDEAS ENTRE UN GRUPO DE PERSONAS","LA EDUCACION EN LINEA ACORTA ___________","SIGLAS DE LA ASOCIACION NACIONAL DE UNIVERSIDADES E INSTITUCIONES DE EDUCACION SUPERIOR","LA EDUCACION EN LINEA REQUIERE UN ENTORNO QUE SEA TOTALMENTE  ____________","INSTITUCION QUE  CREA EL CENTRO DE ALTA TECNOLOGIA EN LA EDUCACION","LA EDUCACION EN LINEA AHORRA Y OPTIMIZA  ___________","SIGLAS DEL SISTEMA DE UNIVERSIDAD ABIERTA"];
+var wDefs=350; posCheck=[];
+var answers=[]; letters=[]; indexLetters=0; xSel=-1; ySel=-1; iLetter=-1; jLetter=-1;
+var wordsGame="U0xFTA"; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var numCols=17; numRows=17; sizeCell=20;
+var direction=["HD","HI","VD","VI","X1D","X1I","X2D","X2I"];
+var boardGame=[[".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".","."]];
+var solved1X=[];solved1Y=[];solved2X=[];solved2Y=[];paintSolved=["N","N","N","N","N","N","N","N","N","N"];x1=[];y1=[];x2=[];y2=[];
+var fillLetter="ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
